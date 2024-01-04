50be3b45e591a404afb457081bb6f8affa3daafa
zekainhuman1 <zekainhuman@gmail.com>
2024-01-04 21:53:23 +0200

    auth2

--- src/container/signup/index.js
+++ src/container/signup/index.js
@@ -1,18 +1,110 @@
-class SignupForm {
-  static value = {}
+import {
+  Form,
+  REG_EXP_EMAIL,
+  REG_EXP_PASSWORD,
+} from '../../script/form'
 
-  static validate = (name, value) => {
-    return true
+class SignupForm extends Form {
+  FIELD_NAME = {
+    EMAIL: 'email',
+    PASSWORD: 'password',
+    PASSWORD_AGAIN: 'passwordAgain',
+    ROLE: 'role',
+    IS_CONFIRM: 'isConfirm',
+  }
+  FIELD_ERROR = {
+    IS_EMPTY: 'Заповніть поле',
+    IS_BIG: 'Занадто багато символів',
+    EMAIL: 'Введіть коректну адресу',
+    PASSWORD: 'Запростий пароль',
+    PASSWORD_AGAIN: 'Паролі не співпадають',
+    NOT_CONFIRM: 'Потрібне погодження с правилами',
+    ROLE: 'Оберіть роль',
   }
 
-  static submit = () => {
-    console.log(this.value)
+  validate = (name, value) => {
+    if (String(value).length < 1) {
+      return this.FIELD_ERROR.IS_EMPTY
+    }
+
+    if (String(value).length > 20) {
+      return this.FIELD_ERROR.IS_BIG
+    }
+
+    if (name === this.FIELD_NAME.EMAIL) {
+      if (!REG_EXP_EMAIL.test(String(value))) {
+        return this.FIELD_ERROR.EMAIL
+      }
+    }
+
+    if (name === this.FIELD_NAME.PASSWORD) {
+      if (!REG_EXP_PASSWORD.test(String(value))) {
+        return this.FIELD_ERROR.PASSWORD
+      }
+    }
+
+    if (name === this.FIELD_NAME.PASSWORD_AGAIN) {
+      if (
+        String(value) !==
+        this.value[this.FIELD_NAME.PASSWORD]
+      ) {
+        return this.FIELD_ERROR.PASSWORD_AGAIN
+      }
+    }
+
+    if (name === this.FIELD_NAME.ROLE) {
+      if (isNaN(value)) {
+        return this.FIELD_ERROR.ROLE
+      }
+    }
+
+    if (name === this.FIELD_NAME.IS_CONFIRM) {
+      if (Boolean(value) !== true) {
+        return this.FIELD_ERROR.NOT_CONFIRM
+      }
+    }
   }
 
-  static change = (name, value) => {
-    console.log(name, value)
-    if (this.validate(name, value)) this.value[name] = value
+  submit = async () => {
+    if (this.disabled === true) {
+      this.validateAll()
+    } else {
+      console.log(this.value)
+
+      this.setAlert('progress', 'Submitting...')
+
+      try {
+        const res = await fetch('/signup', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: this.convertData(),
+        })
+
+        const data = await res.json()
+
+        if (res.ok) {
+          this.setAlert('success', data.message)
+        } else {
+          this.setAlert('error', data.message)
+        }
+      } catch (error) {
+        this.setAlert('error', error.message)
+      }
+    }
+  }
+
+  convertData = () => {
+    return JSON.stringify({
+      [this.FIELD_NAME.EMAIL]:
+        this.value[this.FIELD_NAME.EMAIL],
+      [this.FIELD_NAME.PASSWORD]:
+        this.value[this.FIELD_NAME.PASSWORD],
+      [this.FIELD_NAME.ROLE]:
+        this.value[this.FIELD_NAME.ROLE],
+    })
   }
 }
 
-window.signupForm = SignupForm
+window.signupForm = new SignupForm()
--- src/route/auth.js
+++ src/route/auth.js
@@ -5,6 +5,12 @@ const router = express.Router()
 
 const { User } = require('../class/user')
 
+User.create({
+  email: 'testUser@test.com',
+  password: 123123,
+  role: 1,
+})
+
 // ================================================================
 
 // router.get Створює нам один ентпоїнт
@@ -48,5 +54,28 @@ router.get('/signup', function (req, res) {
   // ↑↑ сюди вводимо JSON дані
 })
 
+router.post('/signup', function (req, res) {
+  const { email, password, role } = req.body
+
+  console.log(req.body)
+
+  if (!email || !password || !role) {
+    return res.status(400).json({
+      message: "Помилк, відсутні обов'язкові поля",
+    })
+  }
+
+  try {
+    User.create({ email, password, role })
+
+    return res.status(200).json({
+      message: 'Користувач зареєстрований',
+    })
+  } catch (err) {
+    return res.status(400).json({
+      message: 'Помилка створення',
+    })
+  }
+})
 // Підключаємо роутер до бек-енду
 module.exports = router
